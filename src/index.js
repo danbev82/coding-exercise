038b4e904fc78b89f9037ea131c644cf67bf319d
Danny Bevan <danbev82@gmail.com>
2021-06-07 19:40:09 +0100

    coding exercise
    coding exercise

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  function groupArrayElements(arrData, colNumber){
+    const arrLen = arrData.length;
+    const groupedArr = [];
+    
+    if(colNumber <=0) {
+      console.log("Please provide a valid number... ");
+    } else {
+  // use math.ceil to round number up then divide the lentgh of array by number of columns
+      const size = Math.ceil(arrLen / colNumber);
+  // slice the array by by the calulated size and push to new array until arrLen < 1
+      for (var i = 0; i < arrLen; i += size) {
+        groupedArr.push(arrData.slice(i, i + size));
+  }                     
+    return groupedArr;
+  }}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//uncomment the below and amend values if required
+
+// const colNumber = 0;
+ const arrData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+// call initial function
+// console.log(groupArrayElements(arrData, colNumber)); 
+   
+
+// comment out test cases f required   
+   
+console.log(groupArrayElements(arrData, 0));  // Please provide a valid number...
+console.log(groupArrayElements(arrData, 1));  // [ [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] ]
+console.log(groupArrayElements(arrData, 2));  // [ [ 1, 2, 3, 4, 5, 6 ], [ 7, 8, 9, 10, 11, 12 ] ]
+console.log(groupArrayElements(arrData, 3));  // [ [ 1, 2, 3, 4 ], [ 5, 6, 7, 8 ], [ 9, 10, 11, 12 ] ]
+console.log(groupArrayElements(arrData, 4));  // [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ], [ 10, 11, 12 ] ] 
+console.log(groupArrayElements(arrData, 5));  // [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ], [ 10, 11, 12 ] ]
+console.log(groupArrayElements(arrData, 6));  // [ [ 1, 2 ], [ 3, 4 ], [ 5, 6 ], [ 7, 8 ], [ 9, 10 ], [ 11, 12 ] ]
---
+++ src/readme.txt
@@ -0,0 +1,8 @@
+
+1. clone repo from git ---
+2. open twigTask.js and amend values:
+    a. uncomment out section to adjust values
+    b. comment out test cases if required
+3. open command line / git bash and cd to twig\src and execute file using node
+  -- node twigTask.js
+4. You will see result printed in console. 
